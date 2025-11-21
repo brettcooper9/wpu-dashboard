d5639e3987312f1ff3c49bc67a95877655135256
Brett Cooper <brett.cooper.j@gmail.com>
2025-11-21 12:57:21 -0800

    trying to add zoom capabilities to graph

--- main_app.py
+++ main_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from wpu.data_loader import read_daily_wpu, read_minute_wpu, read_tick_wpu
-from wpu.processing import merge_resample_forward_fill
+from wpu.processing import merge_resample_forward_fill, filter_by_zoom
 from wpu.plotting import plot_price_line
 
 st.title("WPU Currency Basket Viewer")
@@ -9,6 +9,13 @@ st.title("WPU Currency Basket Viewer")
 daily_file = st.sidebar.file_uploader("Upload daily CSV (10 years)", type=['csv'])
 minute_file = st.sidebar.file_uploader("Upload minute CSV/XLSX (5 days)", type=['csv','xlsx'])
 tick_file = st.sidebar.file_uploader("Upload tick CSV/XLSX (1 day)", type=['csv','xlsx'])
+
+# Zoom selector
+zoom_option = st.sidebar.selectbox(
+    "Select time range",
+    ['1d','5d','1w','1m','3m','6m','1y','3y','5y','10y'],
+    index=0
+)
 
 daily_raw = daily_tidy = None
 minute_raw = minute_tidy = None
@@ -23,7 +30,7 @@ if minute_file is not None:
 if tick_file is not None:
     tick_tidy = read_tick_wpu(tick_file)
 
-# Merge and process
 if daily_tidy is not None:
     merged_df = merge_resample_forward_fill(daily_tidy, minute_tidy, tick_tidy)
-    plot_price_line(merged_df, price_col='price', title="WPUUSD Price")
+    zoomed_df = filter_by_zoom(merged_df, zoom=zoom_option)
+    plot_price_line(zoomed_df, price_col='price', title=f"WPUUSD Price ({zoom_option})")
